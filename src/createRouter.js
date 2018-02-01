a0506c2c39fb119d11eb27838659189e59266e83
Maxwell Sechzer <maxwell.sechzer@persado.com>
2018-02-01 14:40:20 -0500

    add flowtypes, add notFound (404) functionality, and rewrite to not use a map (what was I thinking?)

--- src/createRouter.js
+++ src/createRouter.js
@@ -1,25 +1,36 @@
 // @flow
-import { forOwn } from 'lodash';
+import map from 'lodash/map';
 import { createElement } from 'react';
+import type { ComponentType } from 'react';
 
-export default function createRouter(routes, { regexBuilder = RegExp } = {}) {
-  const regexRoutes = new Map();
-  forOwn(routes, (page, route) => regexRoutes.set(regexBuilder(route), page));
+type Routes = {
+  [string]: ComponentType<*>,
+};
+type RegexBuilder = string => RegExp;
+type Options = {
+  regexBuilder?: RegexBuilder,
+  notFound?: ComponentType<*>,
+};
 
-  return ({ route }) => {
-    const entries = regexRoutes.entries();
-    let routeRegex;
-    let page;
-    let routing;
-    let currentEntry = entries.next();
+function renderByTypeof(component, props) {
+  if (typeof component === 'function') {
+    return createElement(component, props);
+  }
+  return component;
+}
 
-    while (!currentEntry.done) {
-      ({ value: [routeRegex, page] } = currentEntry);
-      routing = routeRegex.exec(route);
-      if (routing) { break; }
-      currentEntry = entries.next();
+export default function createRouter(routes: Routes, { regexBuilder = RegExp, notFound }: Options = {}) {
+  const regexRoutes = map(routes, (page, route) => ({ routeRegex: regexBuilder(route), page }));
+
+  return ({ route }: { route: string }) => {
+    for (let routeIndex = 0; routeIndex < regexRoutes.length; routeIndex++) {
+      const { routeRegex, pageComponent } = regexRoutes[routeIndex];
+      const routing = routeRegex.exec(route);
+      if (routing) {
+        return renderByTypeof(pageComponent, { routing });
+      }
     }
 
-    return createElement(page, { routing });
+    return renderByTypeof(notFound, { route });
   };
 }
