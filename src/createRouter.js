edfd449df4592379016a009f4d38995b57acd361
Maxwell Sechzer <msechzer@fandm.edu>
2018-02-12 14:59:20 -0500

    adds custom matching functionality and iterate over routes in reverse

--- src/createRouter.js
+++ src/createRouter.js
@@ -10,7 +10,12 @@ type RegexBuilder = string => RegExp;
 type Options = {
   regexBuilder?: RegexBuilder,
   notFound?: ComponentType<*>,
+  match?: (RegExp, string) => mixed,
 };
+
+function regexExec(routeRegex, routeString) {
+  return routeRegex.exec(routeString);
+}
 
 function renderByTypeof(component, props) {
   if (typeof component === 'function') {
@@ -19,13 +24,14 @@ function renderByTypeof(component, props) {
   return component;
 }
 
-export default function createRouter(routes: Routes, { regexBuilder = RegExp, notFound }: Options = {}) {
+export default function createRouter(routes: Routes, options: Options = {}) {
+  const { regexBuilder = RegExp, match = regexExec, notFound } = options;
   const regexRoutes = map(routes, (pageComponent, route) => ({ routeRegex: regexBuilder(route), pageComponent }));
 
   return ({ route }: { route: string }) => {
-    for (let routeIndex = 0; routeIndex < regexRoutes.length; routeIndex++) {
+    for (let routeIndex = regexRoutes.length - 1; routeIndex >= 0; routeIndex--) {
       const { routeRegex, pageComponent } = regexRoutes[routeIndex];
-      const routing = routeRegex.exec(route);
+      const routing = match(routeRegex, route);
       if (routing) {
         return renderByTypeof(pageComponent, { routing });
       }
